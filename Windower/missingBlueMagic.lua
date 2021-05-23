6891d9287f73d31ab4ffd6874f64305f19d737d1
Mark Klara <mrhappyasthma@gmail.com>
2021-05-23 16:50:01 -0700

    Check BLU level regardless of main job being set.

--- Windower/missingBlueMagic.lua
+++ Windower/missingBlueMagic.lua
@@ -6,18 +6,12 @@
 local res = require('resources')
 
 local player = windower.ffxi.get_player()
- if not player.main_job == "BLU" then
-   -- TODO: Figure out how to grab the blue level.
-   windower.add_to_chat(123, "BLU must be set as your main job to use this script.")
-   return
- end
-
 local blu_spells = res.spells:type('BlueMagic')
 for key, value in pairs(blu_spells) do
   if not windower.ffxi.get_spells()[value.id] then
     local requiredLevel = value.levels[16]
-    if player.main_job_level >= requiredLevel then
-      windower.add_to_chat(123, "Unlearned Blue Magic Spell: " .. value.english .. " " .. requiredLevel)
+    if player.jobs["BLU"] >= requiredLevel then
+      windower.add_to_chat(123, "Unknown Blue Magic Spell: " .. value.english .. " " .. requiredLevel)
     end
   end
 end
